2e697e2459681efb963d9aced94542a89ed137b1
bomi <a51022866@gmail.com>
2019-07-31 23:19:12 +0900

    #5 Create Table Component

---
+++ src/components/main-section/table/Table.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="table">
+        <div>
+            <section>
+                전체 아이디어 {{ }}건
+            </section>
+            <section>
+                <div>태그검색</div>
+                <div>즐겨찾기만 보기</div>
+                <div>
+                    <input name="query" v-model="searchQuery" placeholder="검색하기">
+                </div>
+            </section>
+        </div>
+        <div>
+            <table class="board">
+                <thead>
+                    <tr>
+                        <th v-for="key in columns"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                <tr v-for="entry in data">
+                    <td v-for="key in columns">
+                        {{ entry[key] }}
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</template>
+
+<script>
+  export default {
+    name: "Table",
+    data() {
+      return {
+        ideas: []
+      }
+    },
+    created() {
+      // axios.get()
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
