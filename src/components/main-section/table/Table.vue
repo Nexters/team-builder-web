1bfd25a51a9b92c8a1a55ab3ededdb200b57d5f4
bomi <a51022866@gmail.com>
2019-08-04 17:48:19 +0900

    #5 Implement title search function and default HTML format

--- src/components/main-section/table/Table.vue
+++ src/components/main-section/table/Table.vue
@@ -1,12 +1,12 @@
+<!-- TO-DO column별로 정렬 기능 구현 -->
+<!-- TO-DO 작성자 검색 기능 구현 -->
 <template>
     <div class="card">
         <div class="card-header">
             <section>
-                전체 아이디어 {{ }}건
+                전체 아이디어 {{ ideaList.length }}건
             </section>
             <section>
-                <div>태그검색</div>
-                <div>즐겨찾기만 보기</div>
                 <div>
                     <input type="search"
                            class="search-input"
@@ -21,9 +21,9 @@
             <table class="board">
                 <thead>
                 <tr>
-                    <th v-if="starFilter === true">즐겨찾기</th>
                     <th v-for="{ name } in titles">{{ name }}</th>
-                    <!--<th>글번호</th>-->
+                    <!--<th>즐겨찾기</th>-->
+                    <!--<th>글 번호</th>-->
                     <!--<th>아이디어 이름</th>-->
                     <!--<th>태그</th>-->
                     <!--<th>직군</th>-->
@@ -32,18 +32,38 @@
                 </tr>
                 </thead>
                 <tbody>
-                <tr v-for="idea in ideaListResult()" :key="idea.id">
-                    <td v-if="starFilter === true">
-                        <input type="checkbox"
-                               name="star-filter"
-                               id="star-filter"
-                               class="star"
-                               :value="idea"
-                               v-model="selected"
-                               @change="select(selected)"
-                        />
+                <tr v-for="idea in ideaListResult()" :key="idea.orderNumber">
+                    <!-- table column의 index, favorite이 0으로 시작 -->
+                    <td v-for="(value, index) in titles">
+                        <!-- favorite -->
+                        <div v-if="index === 0">
+                            <input type="checkbox"
+                                   name="favorite-filter"
+                                   id="favorite-filter"
+                                   class="favorite"
+                                   :value="idea"
+                                   v-model="selected"
+                                   @change="select(selected)"
+                            />
+                        </div>
+                        <!-- tags -->
+                        <div v-else-if="index === 3">
+                            <div v-for="{ name } in idea[value.prop]">
+                                {{ name }}
+                            </div>
+                        </div>
+                        <!-- position -->
+                        <div v-else-if="index === 4">
+                            {{ idea[value.prop].position }}
+                        </div>
+                        <!-- author -->
+                        <div v-else-if="index === 5">
+                            {{ idea[value.prop].name }}
+                        </div>
+                        <div v-else>
+                            {{ idea[value.prop] }}
+                        </div>
                     </td>
-                    <td v-for="{ prop } in titles">{{ idea[prop] }}</td>
                 </tr>
                 </tbody>
             </table>
@@ -59,22 +79,18 @@
       event: "change"
     },
     props: {
-      starFilter: {
-        type: Boolean,
-        default: true
-      },
       ideaList: {
         type: Array,
         default: function () {
           return [];
         }
       },
-      searchAttrs: {
-        type: Array,
-        default: function () {
-          return [];
-        }
-      },
+      // searchAttrs: {
+      //   type: Array,
+      //   default: function () {
+      //     return [];
+      //   }
+      // },
       titles: {
         type: Array,
         default: function() {
@@ -104,23 +120,30 @@
       ideaListResult() {
         return this.ideas;
       },
+
       filterData() {
-        console.log("come in");
         this.searchTerm = this.searchTerm.toLowerCase();
-        console.log("검색어 " + this.searchTerm);
         if(this.searchTerm === "") {
           this.ideas = this.ideaList;
           return;
         }
 
-        console.log("not returned");
         this.ideas = this.ideaList.filter(idea =>
-            this.searchAttrs.some(search =>
-                idea[search].toLowerCase().includes(this.searchTerm)
+        //   ["title", "author"].some(function (search) {
+        //     if(search === "title") {
+        //       idea[search].toLowerCase().includes(this.searchTerm)
+        //     } else {
+        //       idea[search].name.toLowerCase().includes(this.searchTerm)
+        //     }
+        //   })
+        // );
+            ["title"].some(search =>
+              idea[search].toLowerCase().includes(this.searchTerm)
             )
         );
         console.log(this.ideas);
       },
+
       select(ideas) {
         this.ideas = ideas;
         console.log(ideas);
