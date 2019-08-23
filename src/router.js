3ce62f76ec4599663dc44c5b08fc025fabbed815
leekwanho <rhksgh7492@naver.com>
2019-08-23 20:34:20 +0900

    BlockPage
    Block all page without login and signup

--- src/router.js
+++ src/router.js
@@ -21,6 +21,7 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
+      beforeEnter: requireAuth(),
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
@@ -41,46 +42,55 @@ export default new Router({
     {
       path: '/example',
       name: 'Example',
+      beforeEnter: requireAuth(),
       component: () => import(/* webpackChunkName: "example" */ './views/Example.vue')
     },
     {
       path: '/info',
       name: 'Info',
+      beforeEnter: requireAuth(),
       component: () => import(/* webpackChunkName: "editor" */ './views/info/Info.vue')
     },
     {
       path: '*',
       name: 'NotFound',
+      beforeEnter: requireAuth(),
       component: () => import(/* webpackChunkName: "NotFound" */ './views/NotFound.vue')
     },
     {
       path: '/session/:sessionNumber',
       name: 'Session',
+      beforeEnter: requireAuth(),
       component: () => import(/* webpackChunkName: "Main" */ './views/Main.vue'),
     },
     {
       path: '/session/:sessionNumber/idea/new',
       name: 'NewIdea',
+      beforeEnter: requireAuth(),
       component: () => import(/* webpackChunkName: "Main" */ './views/NewIdea.vue'),
     },
     {
       path: '/session/:sessionNumber/idea/:ideaId',
       name: 'IdeaDetail',
+      beforeEnter: requireAuth(),
       component: () => import(/* webpackChunkName: "Main" */ './views/IdeaDetail.vue'),
     },
     {
       path: '/all-user-manager',
       name: 'AllUserManage',
+      beforeEnter: requireAuth(),
       component: () => import(/* webpackChunkName: "Main" */ './views/admin/AllUserManage.vue'),
     },
     {
       path: '/session/:sessionNumber/general-manage',
       name: 'GeneralManage',
+      beforeEnter: requireAuth(),
       component: () => import(/* webpackChunkName: "Main" */ './views/admin/GeneralManage.vue'),
     },
     {
       path: '/session/:sessionNumber/user-manage',
       name: 'ActiveUserManage',
+      beforeEnter: requireAuth(),
       component: () => import(/* webpackChunkName: "Main" */ './views/admin/ActiveUserManage.vue'),
     },
   ]
