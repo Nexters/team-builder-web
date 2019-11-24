c65e9f36856d2353c177ee6aaa19a93650a921c1
leekwanho <rhksgh7492@naver.com>
2019-11-24 17:29:29 +0900

    nofeature/FixedLoginErrorMessage
    Delete error message in login page when login fail.

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -87,7 +87,7 @@
                             })
                     })
                     .catch(err => {
-                        alert('아이디와 비밀번호를 확인해주세요.', err);
+                        alert('아이디와 비밀번호를 확인해주세요.');
                         this.duringLogin = false
                     });
 
